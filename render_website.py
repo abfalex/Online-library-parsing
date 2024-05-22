fd71d33e87345c404524c0f2deeb731dada97912
abfalex <87718997+abfalex@users.noreply.github.com>
2024-05-22 18:24:02 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+import os
 import json
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
@@ -10,20 +11,30 @@ env = Environment(
 )
 
 def on_reload():
-    template = env.get_template('template.html')
+    page_directory = "pages"
+
+    os.makedirs(page_directory, exist_ok=True)
+
+    page_template = env.get_template('page_template.html')
     json_path = "assets/books/books.json"
 
     with open(json_path, "r", encoding="UTF-8") as f:
         json_file = f.read()
 
     books_details = json.loads(json_file)
+    books_per_column = 8
+    book_groups = list(chunked(books_details, 2))
+    pages_groups = list(chunked(book_groups, books_per_column))
 
-    rendered_page = template.render(
-        books=list(chunked(books_details, 2))
-    )
+    for page_number, pages_groups in enumerate(pages_groups, start=1):
+        page_output_path = os.path.join(page_directory, f"index{page_number}.html")
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+        rendered_page_template = page_template.render(
+            pages_groups=pages_groups
+        )
+
+        with open(page_output_path, "w", encoding="UTF-8") as f:
+            f.write(rendered_page_template)
 
 
 if __name__ == "__main__":
