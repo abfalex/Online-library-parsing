d921bcd1d0d5ad31644d53e94e644356f93873dc
abfalex <87718997+abfalex@users.noreply.github.com>
2024-05-29 12:56:31 +0300

    Updated main functionality

--- render_website.py
+++ render_website.py
@@ -3,33 +3,47 @@ import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
+import argparse
 
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
 def on_reload():
-    page_directory = "pages"
+    parser = argparse.ArgumentParser(
+        description="The program receives data about books that will be displayed on the web pages"
+    )
+    
+    parser.add_argument(
+        "-jp",
+        "--json_path",
+        type=str,
+        help="Path to the json file with book details",
+        default="books.json"
+    )
 
-    os.makedirs(page_directory, exist_ok=True)
+    args = parser.parse_args()
+    json_path = args.json_path
 
-    page_template = env.get_template('page_template.html')
-    json_path = "assets/books/books.json"
+    env = Environment(
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html", "xml"])
+    )
+
+    template = env.get_template("template.html")
+    
+    pages_directory = "pages"
+    os.makedirs(pages_directory, exist_ok=True)
 
     with open(json_path, "r", encoding="UTF-8") as f:
-        json_file = f.read()
+        books_details = json.load(f)
 
-    books_details = json.loads(json_file)
-    books_per_column = 8
-    book_groups = list(chunked(books_details, 2))
-    pages_groups = list(chunked(book_groups, books_per_column))
+    number_book_columns = 8
+    row_books_number = 2
+    book_groups = list(chunked(books_details, row_books_number))
+    pages_groups = list(chunked(book_groups, number_book_columns))
 
     for page_number, pages_group in enumerate(pages_groups, start=1):
-        page_output_path = os.path.join(page_directory, f"index{page_number}.html")
+        page_output_path = os.path.join(pages_directory, f"index{page_number}.html")
 
-        rendered_page_template = page_template.render(
+        rendered_page_template = template.render(
             pages_group=pages_group,
             page_number=page_number,
             total_pages=len(pages_groups)
@@ -44,4 +58,4 @@ if __name__ == "__main__":
     
     server = Server()
     server.watch("page_template.html", on_reload)
-    server.serve(root='.')
+    server.serve(root=".")
